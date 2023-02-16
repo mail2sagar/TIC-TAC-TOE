5e6fc91f6ff9e23de1b0b11ba5d98b8867c3c5ab
mail2sagar <36476038+mail2sagar@users.noreply.github.com>
2023-02-16 17:32:04 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+let playerText = document.getElementById('playerText')
+let restartBtn = document.getElementById('restartBtn')
+let boxes = document.getElementsByClassName('box')
+
+
+
+const O_TEXT = "O"
+const X_TEXT = "X"
+// mokshitha X and shital O
+let currentPlayer = X_TEXT
+let spaces = Array(9).fill(null)
+
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener('click', boxClicked))
+}
+
+function boxClicked(e) {
+    const id = e.target.id
+
+    if (!spaces[id]) {
+        spaces[id] = currentPlayer
+        e.target.innerText = currentPlayer
+
+        if (playerHasWon()) {
+            playerText = '${currentPlayer} has won'
+        }
+
+        // Ternary operator / if else
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
+    }
+}
+
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+function playerHasWon() {
+
+}
+
+restartBtn.addEventListener('click', restart)
+
+function restart() {
+    spaces.fill(null)
+
+    boxes.forEach(box => {
+        box.innerText = ''
+    })
+
+    playerText = 'Tic Tac Toe'
+
+    currentPlayer = X_TEXT
+}
+
+startGame()
